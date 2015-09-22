c495d42c3dd7883ebb864ed8a4b571acebd5ab29
88rabbit <ljj88tu1988@gmai.com>
2015-09-22 09:32:02 -0500

    no test

--- ci/travis.rb
+++ ci/travis.rb
@@ -61,7 +61,7 @@ class Build
     if activerecord?
       ['db:mysql:rebuild', "#{adapter}:#{'isolated_' if isolated?}test"]
     else
-      ["test", ('isolated' if isolated?), ('integration' if integration?)].compact.join(":")
+      [('isolated' if isolated?), ('integration' if integration?)].compact.join(":")
     end
   end
 
